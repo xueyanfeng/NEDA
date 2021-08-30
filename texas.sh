1a7669c2e9a26a57e58f7d4fc2464e46760c90df
xueyanfeng <xueyanfeng0819@qq.com>
2021-08-30 18:01:22 +0800

    Add files via upload

---
+++ texas.sh
@@ -0,0 +1,3 @@
+mkdir results
+python train_NEDA.py --no-cuda --dataset texas --sample1 24 --sample2 3 --infected_number 36
+python train_NEDA.py --no-cuda --is_copy --dataset texas --sample1 18 --sample2 3 --infected_number 36
