100c67662288b0a82faf5fa02ce1641b0c3435a3
xueyanfeng <xueyanfeng0819@qq.com>
2021-08-31 09:22:11 +0800

    Add files via upload

---
+++ cornell.sh
@@ -0,0 +1,3 @@
+mkdir results
+python train_NEDA.py --no-cuda --dataset cornell --sample1 24 --sample2 9 --infected_number 36
+python train_NEDA.py --no-cuda --is_copy --dataset cornell --sample1 18 --sample2 12 --infected_number 36
