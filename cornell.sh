7871692736cafedd3f6db607f0d80450b5f0a257
xueyanfeng <xueyanfeng0819@qq.com>
2022-03-11 17:23:07 +0800

    Delete cornell.sh

--- cornell.sh
+++
@@ -1,2 +0,0 @@
-python train_NEDA.py --no-cuda --dataset cornell --sample1 24 --sample2 9 --infected_number 36
-python train_NEDA.py --no-cuda --is_copy --dataset cornell --sample1 18 --sample2 12 --infected_number 36
