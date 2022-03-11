806789c23c58f1227b8a60126e1b1651cbfe0576
xueyanfeng <xueyanfeng0819@qq.com>
2022-03-11 17:23:18 +0800

    Delete film.sh

--- film.sh
+++
@@ -1,2 +0,0 @@
-python train_NEDA.py --no-cuda --dataset film --sample1 25 --sample2 10 --infected_number 25
-python train_NEDA.py --no-cuda --is_copy --dataset film --sample1 25 --sample2 20 --infected_number 25
