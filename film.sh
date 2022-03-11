f1296e17f31cd97449dbd8901ad3790df97b2e8c
xueyanfeng <xueyanfeng0819@qq.com>
2022-03-12 07:52:40 +0800

    Update film.sh

--- film.sh
+++ film.sh
@@ -1,4 +1,4 @@
 python train_NEDA.py --no-cuda --similarity --dataset film --sample1 25 --sample2 10 --infected_number 25
 python train_NEDA.py --no-cuda --similarity --is_copy --dataset film --sample1 25 --sample2 20 --infected_number 25
 python train_NEDA.py --no-cuda --similarity --dataset film --sample1 25 --sample2 10 --infected_number 25 --train_percentage 0.48 --val_percentage 0.32
-python train_NEDA.py --no-cuda --similarity --is_copy --dataset film --sample1 25 --sample2 20 --infected_number 25 --train_percentage 0.48 --val_percentage 0.32
+python train_NEDA.py --no-cuda --similarity --is_copy --dataset film --sample1 15 --sample2 15 --infected_number 23 --train_percentage 0.48 --val_percentage 0.32
