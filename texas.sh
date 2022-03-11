1d753e5108e95391895b972044a4f4e892104cef
xueyanfeng <xueyanfeng0819@qq.com>
2022-03-11 17:23:41 +0800

    Delete texas.sh

--- texas.sh
+++
@@ -1,2 +0,0 @@
-python train_NEDA.py --no-cuda --dataset texas --sample1 24 --sample2 3 --infected_number 36
-python train_NEDA.py --no-cuda --is_copy --dataset texas --sample1 18 --sample2 3 --infected_number 36
