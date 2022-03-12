16b169ab52e5bd7377b121e49092df4118d6a1f3
xueyanfeng <xueyanfeng0819@qq.com>
2022-03-12 08:17:29 +0800

    Update wisconsin.sh

--- wisconsin.sh
+++ wisconsin.sh
@@ -1,4 +1,4 @@
 python train_NEDA.py --no-cuda --similarity --dataset wisconsin --sample1 21 --sample2 9 --infected_number 37
 python train_NEDA.py --no-cuda --similarity --is_copy --dataset wisconsin --sample1 21 --sample2 3 --infected_number 37
-python train_NEDA.py --no-cuda --similarity --dataset wisconsin --sample1 21 --sample2 9 --infected_number 37 --train_percentage 0.48 --val_percentage 0.32
-python train_NEDA.py --no-cuda --similarity --is_copy --dataset wisconsin --sample1 21 --sample2 3 --infected_number 37 --train_percentage 0.48 --val_percentage 0.32
+python train_NEDA.py --no-cuda --similarity --dataset wisconsin --sample1 6 --sample2 21 --infected_number 37 --train_percentage 0.48 --val_percentage 0.32
+python train_NEDA.py --no-cuda --similarity --is_copy --dataset wisconsin --sample1 3 --sample2 21 --infected_number 37 --train_percentage 0.48 --val_percentage 0.32
